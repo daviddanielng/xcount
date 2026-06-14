ad4cafa0a8e5bac77c1fa0a8ef0133f71e0e2b6b
Daniel Enubiaka <danielenubiakamail@gmail.com>
2026-06-14 20:15:52 +0100

    Add contributing guidelines and MIT license

--- try.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#cargo run -- --username shs,jss,jsj
-#cargo run -- --input ./ttt
-cargo run -- --input ./ttt -f json -o ./outputs
-#cargo run -- --set-exe-path /usr/bin/google-chrome
